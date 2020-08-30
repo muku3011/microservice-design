21c977b3154156fe70323c09ec5f071c70bb9b10
Kusum Negi <kusumnegi2006@gmail.com>
2020-08-30 17:41:12 +0200

    minor refactoring

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# spring-zuul
+# Spring-Zuul-Config-Eureka-Client
+
+
+Config server can source the properties from 2 sources:
+* Local file system (profile = native) 
+* External source control
+
+In our case it is github (profile = git). There are other sources like bitbucket, which are supported as well.
+
+##### Eureka UI:
+http://localhost:8761/
+
+##### Spring Boot Actuator:
+https://docs.spring.io/spring-boot/docs/current/reference/html/production-ready-features.html#production-ready-enabling
+
+You can choose to manage and monitor your application by using **HTTP endpoints** or with **JMX**.
+**Auditing, health, and metrics gathering** can also be automatically applied to your application.
+
--- zuul-service/src/main/java/com/springboot/controller/Controller.java
+++ zuul-service/src/main/java/com/springboot/controller/Controller.java
@@ -1,20 +1,26 @@
 package com.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/api")
 public class Controller {
 
-	@GetMapping
-	public ResponseEntity<String> insertStudentDetails() {
+    @Value("${properties.name:default}")
+    private String name;
 
-		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    @GetMapping
+    public ResponseEntity<String> insertStudentDetails() {
 
-		return new ResponseEntity<>("{\"Demo\": \"Completed\"}", HttpStatus.OK);
-	}
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+        return new ResponseEntity<>("{\"Demo\": " + name + "}", HttpStatus.OK);
+    }
 }
