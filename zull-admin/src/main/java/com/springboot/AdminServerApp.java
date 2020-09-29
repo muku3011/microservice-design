22eb08f4fda30949b73b9259c313e56d507c6e71
Mukesh Joshi <mukesh.bciit@gmail.com>
2020-09-29 22:23:06 +0200

    admin server added

---
+++ zull-admin/src/main/java/com/springboot/AdminServerApp.java
@@ -0,0 +1,15 @@
+package com.springboot;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class AdminServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApp.class, args);
+    }
+}
